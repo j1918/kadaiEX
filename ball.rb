0a9a5534e3d3b422aa2dd16ea980f8bf6496e8d8
j1918 <j1918@matsue-ct.ac.jp>
2022-09-17 13:37:48 +0900

    更新

--- ball.rb
+++ ball.rb
@@ -1,7 +1,7 @@
 require_relative 'bar'
 class Ball < Sprite
     #attr_accessor :score # 追加
-    REDIUS = 16
+    REDIUS = 12
 
     def initialize(x, y, image) # 追加
       @x_vector = 2
@@ -18,23 +18,27 @@ class Ball < Sprite
         @x_vector *= -1
       elsif(self.x < 0 + REDIUS)
         @x_vector *= -1
-      elsif(self.y > 480 - REDIUS)
-        @y_vector *= -1
       elsif(self.y < 0 + REDIUS)
         @y_vector *= -1
+      #elsif(self.y > 480 - REDIUS)
+        #@y_vector *= -1
       end
     end
 
-    def side_hit(x, y)
-      if(self.x > x && @x_vector > 0 && self.y + REDIUS > y && self.y + REDIUS < y + Bar::BAR_SIZE_Y)
-        @x_vector *= -1
-      elsif(self.x < x && @x_vector < 0 && self.y - REDIUS > y && self.y - REDIUS < y + Bar::BAR_SIZE_Y)
-        @x_vector *= -1
+    def gameover?
+      if(self.y > 480)
+        return true
+      else
+        return false
       end
     end
-  
-    def hit
-      @y_vector *= -1
+
+    def hit(x, y)
+      if(self.y + REDIUS > y && self.y + REDIUS < y + 3 && @y_vector > 0)
+        if(self.x > x && self.x < x + Bar::BAR_SIZE_X)
+          @y_vector *= -1
+        end
+      end
     end
 
     def shot
--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ require_relative 'block'
 
 font = Font.new(24)
 bar = Bar.new(320, 400, nil)
-ball = Ball.new(320, 400, nil)
+ball = Ball.new(320 + Bar::BAR_SIZE_X / 2, 400 - Ball::REDIUS, nil)
 blocks = []
 
 for i in 0..7
@@ -16,15 +16,27 @@ for i in 0..7
 end
 
 $score = 0
+gameover_flag = false
 
 Window.loop do
-  bar.update
-  ball.update
+  font = Font.new(64)
+  Window.draw_font(170, 200, "ブロック崩し", font)
+  font = Font.new(32)
+  Window.draw_font(190, 270, "PRESS ENTER!!", font)
+  if(Input.keyDown?(K_RETURN) == true)
+    break
+  end
+end
 
-  Window.draw_circle_fill(ball.x, ball.y, Ball::REDIUS, C_WHITE)
-  Window.draw_box_fill(bar.x, bar.y, bar.x + Bar::BAR_SIZE_X, bar.y + Bar::BAR_SIZE_Y, C_MAGENTA)
-  blocks.each_with_index do |block, i|
-    if(!block.vanished?)
+Window.loop do
+  if(!gameover_flag)
+    bar.update
+    ball.update
+
+    Window.draw_circle_fill(ball.x, ball.y, Ball::REDIUS, C_WHITE)
+    Window.draw_box_fill(bar.x, bar.y, bar.x + Bar::BAR_SIZE_X, bar.y + Bar::BAR_SIZE_Y, C_MAGENTA)
+    blocks.each_with_index do |block, i|
+      if(!block.vanished?)
         case i % 4
         when 0 
             Window.draw_box_fill(block.x, block.y, block.x + Block::BLOCK_SIZE_X, block.y + Block::BLOCK_SIZE_Y, C_RED)
@@ -35,12 +47,23 @@ Window.loop do
         when 3
             Window.draw_box_fill(block.x, block.y, block.x + Block::BLOCK_SIZE_X, block.y + Block::BLOCK_SIZE_Y, C_GREEN)
         end
+      end
     end
+
+    Window.draw_font(10, 0, "SCORE: #{$score}", font)
+
+    ball.hit(bar.x, bar.y)
+    Sprite.check(ball, blocks)
+    gameover_flag = ball.gameover?
+  elsif($score >= 400)
+    font = Font.new(64)
+    Window.draw_font(150, 200, "Game Clear!!", font)
+    font = Font.new(32)
+    Window.draw_font(250, 270, "score : #{$score}", font)
+  else
+    font = Font.new(64)
+    Window.draw_font(170, 200, "Game Over", font)
+    font = Font.new(32)
+    Window.draw_font(250, 270, "score : #{$score}", font)
   end
-
-  Window.draw_font(10, 0, "SCORE: #{$score}", font)
-
-  #ball.side_hit(bar.x, bar.y)
-  Sprite.check(bar, ball)
-  Sprite.check(ball, blocks)
 end
